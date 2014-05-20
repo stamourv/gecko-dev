452bee90c463c72b99038556f0942cb1e3b4de96
Vincent St-Amour <stamourv@racket-lang.org>
2014-05-20 10:55:03 -0700

    Avoid printing non-existent types.

--- js/src/jit/IonBuilder.cpp
+++ js/src/jit/IonBuilder.cpp
@@ -8643,7 +8643,7 @@ IonBuilder::jsop_getprop(PropertyName *name)
             script()->filename(), script()->lineno(),
             script()->id(), script()->pcToOffset(pc));
     fprintf(stderr, "COACH:    types:");
-    types->print();
+    if (types) types->print();
     fprintf(stderr, "\n");
 
     // Try to hardcode known constants.
